fe3f63c33a58857477aae9b36834b2243cad69fe
qizhemayidema <790353029@qq.com>
2019-08-07 18:02:30 +0800

    项目初始化

---
+++ config/admin/template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘彪
+ * Date: 2019/7/22
+ * Time: 14:46
+ */
+
+return [
+    'tpl_replace_string'  =>  [
+        '__STATIC__'=>'/static/admin',
+    ]
+];
