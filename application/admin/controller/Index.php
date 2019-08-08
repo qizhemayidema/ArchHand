28f68000bd90f23c0813288ee035270c7426956c
bai <450175191@qq.com>
2019-08-08 09:30:55 +0800

    获取文库分类

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,7 +7,10 @@ use think\Controller;
 class Index extends Controller
 {
     public function index(){
-        
+$data  = getCategoryTree();
+//return json($data);
+        dump($data);die;
+
         return $this->fetch();
     }
 }
--- application/common.php
+++ application/common.php
@@ -10,3 +10,31 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+use app\admin\model\LibraryCategory;
+
+
+/**
+ * 文库树形菜单
+ * @param int $parentId
+ * @param null $allCategories
+ * @return array
+ * @throws \think\Exception\DbException
+ */
+function getCategoryTree($parentId = 0, $allCategories = null)
+{
+    if (is_null($allCategories)) {
+        //从数据库中一次性取出所有类目
+        $allCategories = LibraryCategory::all();
+    }
+    $data = array();
+    $categories = LibraryCategory::where('p_id',$parentId)->all();
+    foreach($categories as $k=>$v){
+        $data[$k] = ['id' => $v->id, 'name' => $v->cate_name];
+        if($v->p_id!=0){
+            return $data;
+        }
+        $data[$k]['children']=getCategoryTree($v->id,$allCategories);
+
+    }
+    return $data;
+}
