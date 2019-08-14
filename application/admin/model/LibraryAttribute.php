ef19118eef12a64945b0068fab5b8f46a295934d
qizhemayidema <790353029@qq.com>
2019-08-14 11:25:03 +0800

    文库属性

--- application/admin/model/LibraryAttribute.php
+++ application/admin/model/LibraryAttribute.php
@@ -7,4 +7,9 @@ use think\Model;
 class LibraryAttribute extends Model
 {
     //
+
+//    public function getList()
+//    {
+//        return $this->hasMany(LibraryAttributeValue::class,'attr_id','id');
+//    }
 }
